285a526f1ae264bfe13c6fd7163451f8eeeb066a
Esteits <thomascourvoisier0@gmail.com>
2026-04-16 20:56:25 +0200

    Ajout du jar + rend propre le respawn du Patrouilleur

--- core/src/main/java/com/loderunner/project/engine/Game.java
+++ core/src/main/java/com/loderunner/project/engine/Game.java
@@ -231,6 +231,9 @@ public class Game implements Serializable{
                 this.score += 10;
                 e.setFree(false);
                 e.setTimeToRespawn(25);
+                if(e instanceof EnemyPatrouilleur){
+                    e.setTimeToRespawn(100);
+                }
                 return;
             }
         }
@@ -302,13 +305,8 @@ public class Game implements Serializable{
             if(!e.getFree()){
                 e.setTimeToRespawn(e.getTimeToRespawn() - 1);
                 if(e.getTimeToRespawn() <= 0){
-                    if(e instanceof EnemyPatrouilleur){
-                        e.respawn(e.getX(), e.getY() - 1);
-                        e.setFree(true);
-                    }else{
-                        e.respawn(maze.getExit(), 0);
-                        e.setFree(true);
-                    }
+                    e.respawn(maze.getExit(), 0);
+                    e.setFree(true);
                 }
             }
         }
